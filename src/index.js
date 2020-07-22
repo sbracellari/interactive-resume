a07143f32d7b36a7105ad468c1ca1b275af4f257
Samantha Bracellari <sbracellari@gmail.com>
2020-07-22 19:58:07 -0400

    initial commit

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './App'
+
+const project_name = 'interactive-resume'
+
+ReactDOM.render(
+  <App />,
+  document.getElementById(project_name)
+)
